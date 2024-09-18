944aa618cd0f3ef546754bd854eab82686e53ec5
Usmanalisd <muhammadusmanalisd@outlook.com>
2024-09-19 02:20:58 +0500

    Initial Commit

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+const SingIn = () => {
+  return (
+    <div>
+      <h1>Sign Inn</h1>
+    </div>
+  );
+};
+
+export default SingIn;
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-blue-500">Hello LMS in NEXT</p>
+      <Button variant="secondary" className="my-4">
+        {" "}
+        Click for LMS
+      </Button>
+    </div>
+  );
+}
